105c8fba0fcaa008d5e65d92967367a08e29457c
ashok997 <ashok997@gmail.com>
2019-12-28 11:54:51 -0600

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleUpcase);
 }
+
+function titleUpcase(title) {
+  let word = title.split(" ");
+  return word.map(upCase).join(" ");
+}
+function upCase(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
